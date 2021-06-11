5b211fbaebc0c9605f9abb45680fdaf474a05a4b
Ross Keenan <rossk29@gmail.com>
2021-06-11 09:50:27 +0200

    Refactor utility functions

---
+++ utility.ts
@@ -0,0 +1,77 @@
+// Functions
+/// Source: https://stackoverflow.com/questions/36721830/convert-hsl-to-rgb-and-hex
+export function hslToHex(h: number, s: number, l: number) {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+export function hexToHSL(hex: string) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+  var r = parseInt(result[1], 16);
+  var g = parseInt(result[2], 16);
+  var b = parseInt(result[3], 16);
+
+  (r /= 255), (g /= 255), (b /= 255);
+  var max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  var h,
+    s,
+    l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  s = s * 100;
+  s = Math.round(s);
+  l = l * 100;
+  l = Math.round(l);
+  h = Math.round(360 * h);
+
+  return [h, s, l];
+}
+export function convertDataURIToBinary(dataURI: string) {
+  const base64Index: number = dataURI.indexOf(";base64,") + ";base64,".length;
+  const base64: string = dataURI.substring(base64Index);
+  const raw = window.atob(base64);
+  const rawLength = raw.length;
+  let array = new Uint8Array(new ArrayBuffer(rawLength));
+  for (let i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return array;
+}
+export function sumRows(array: number[][]) {
+  const result: number[] = [];
+  array.forEach((row, i) => {
+    row.reduce((a, b) => (result[i] = a + b));
+  });
+  return result;
+}
+export function normalise(array: number[]) {
+  const max = Math.max(...array);
+  return array.map((x) => x / max);
+}
